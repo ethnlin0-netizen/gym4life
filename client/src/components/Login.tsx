ff84f3177154f3c635a9641c3ace6635b49044cf
Ethan Lin <ethnlin0@gmail.com>
2026-07-26 01:19:25 -0400

    added return statements for login + register

--- client/src/components/Login.tsx
+++ client/src/components/Login.tsx
@@ -21,7 +21,7 @@ function Login() {
             localStorage.setItem('token', response.data.token)
             localStorage.setItem('user', JSON.stringify(response.data.user))
             navigate('/dashboard')
-            
+
         } catch (error) {
             setMessage('Server error, please try again')
             setMessageType('error');
@@ -30,7 +30,22 @@ function Login() {
 
     return (
         <div>
-
+            <form onSubmit={handleSubmit}>
+                <input
+                    type="text"
+                    placeholder="Login"
+                    value={login}
+                    onChange={(e) => setLogin(e.target.value)}
+                />
+                <input
+                    type="text"
+                    placeholder="Password"
+                    value={password}
+                    onChange={(e) => setPassword(e.target.value)}
+                />
+                <button type="submit">Login</button>
+            </form>
+            {message && <p>{message}</p>}
         </div>
     )
 }
--- client/src/components/Register.tsx
+++ client/src/components/Register.tsx
@@ -58,7 +58,39 @@ function Register() {
 
     return (
         <div>
-
+            <form onSubmit={handleSubmit}>
+                <input
+                    type="text"
+                    placeholder="First Name"
+                    value={firstName}
+                    onChange={(e) => setFirstName(e.target.value)}
+                />
+                <input
+                    type="text"
+                    placeholder="Last Name"
+                    value={lastName}
+                    onChange={(e) => setLastName(e.target.value)}
+                />
+                <input
+                    type="text"
+                    placeholder="Email"
+                    value={email}
+                    onChange={(e) => setEmail(e.target.value)}
+                />
+                <input
+                    type="text"
+                    placeholder="Login"
+                    value={login}
+                    onChange={(e) => setLogin(e.target.value)}
+                />
+                <input
+                    type="text"
+                    placeholder="Password"
+                    value={password}
+                    onChange={(e) => setPassword(e.target.value)}
+                />
+            </form>
+            {message && <p>Message</p>}
         </div>
     )
 }
