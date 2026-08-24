e922955e03adafb478b75b48fcce54693b69c8de
Ethan Lin <ethnlin0@gmail.com>
2026-08-23 20:51:56 -0400

    tailwind setup

--- client/src/pages/LandingPage.tsx
+++ client/src/pages/LandingPage.tsx
@@ -6,12 +6,20 @@ const LandingPage = () => {
     const [activeTab, setActiveTab] = useState<string>('register')
 
     return(
-        <div>
-            <button onClick={() => setActiveTab('register')}>Register</button>
-            <button onClick={() => setActiveTab('login')}>Login</button>
-            
-            {activeTab === 'register' && <Register />}
-            {activeTab === 'login' && <Login />}
+        <div className="flex h-screen">
+            <div className="w-1/2">
+                <h1>GYM4LIFE</h1>
+                <p>Create and track workouts that count.</p>
+            </div>
+            <div className="w-1/2">
+                <div>
+                    <h1>Get Started</h1>
+                    <button onClick={() => setActiveTab('register')}>Register</button>
+                    <button onClick={() => setActiveTab('login')}>Login</button>
+                    {activeTab === 'register' && <Register />}
+                    {activeTab === 'login' && <Login />}
+                </div>
+            </div>
         </div>
     )
 }
